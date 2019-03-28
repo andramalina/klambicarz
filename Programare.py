be8b3ff0c29bfe8cf989a24e50485a09e725b808
mire <mire.linul@yahoo.com>
2019-03-28 13:30:42 +0200

    miree. adds class for Programare

---
+++ Programare.py
@@ -0,0 +1,52 @@
+import datetime
+
+
+class Programare():
+    def __init__(self, id_programare, id_parcare, nr_inmatriculare, data_programare, ora_sosire, ora_plecare,
+                 tip_incarcare):
+        self.id_programare: int = id_programare
+        self.id_parcare: str = id_parcare
+        self.nr_inmatriculare: str = nr_inmatriculare
+        self.data_programare: datetime = data_programare
+        self.ora_sosire: datetime.time() = ora_sosire
+        self.ora_plecare: datetime.time() = ora_plecare
+        self.tip_incarcare: str = tip_incarcare
+
+    def set_id_programare(self, idProgramare):
+        self.id_programare = idProgramare
+
+    def set_id_parcare(self, idParcare):
+        self.id_parcare = idParcare
+
+    def set_nr_inmatriculare(self, nrInm):
+        self.nr_inmatriculare = nrInm
+
+    def set_data_programare(self, dataProgramare):
+        self.data_programare = dataProgramare
+
+    def set_ora_sosire(self, oraSosire):
+        self.ora_sosire = oraSosire
+
+    def set_ora_plecare(self, oraPlecare):
+        self.ora_plecare = oraPlecare
+
+    def set_tip_programare(self, tipProgramare):
+        self.tip_incarcare = tipProgramare
+
+    def get_id_programare(self):
+        return self.id_programare
+
+    def get_id_parcare(self):
+        return self.id_parcare
+
+    def get_nr_inmatriculare(self):
+        return self.nr_inmatriculare
+
+    def get_ora_sosire(self):
+        return self.ora_sosire
+
+    def get_ora_plecare(self):
+        return self.ora_plecare
+
+    def get_tip_incarcare(self):
+        return self.tip_incarcare
