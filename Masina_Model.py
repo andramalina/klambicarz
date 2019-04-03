9363e80155317e085c3ad4d05d3150133e8b17be
Karina <karinalar1@yahoo.com>
2019-04-03 19:01:00 +0300

    karina's classes

--- Masina_Model.py
+++ Masina_Model.py
@@ -1,2 +1,11 @@
-class Masina_Model():
-    pass
+import Basic_Model
+class Masina_Model(Basic_Model):
+    def __init__(self):
+        Basic_Model.__init__(self)
+
+    def viewAllMasiniForUser(self, user):
+        id = user.get_id_user()
+        select = "select * from masina where id_user=%s "
+        val = (id)
+        self.cursor.execute(select, val)
+        self.db.commit()
